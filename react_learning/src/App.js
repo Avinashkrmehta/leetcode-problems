7ac7716c5c5d0aa0fb38c4c2d889bff3c4a595d3
Avinashkrmehta <avi4791@gmail.com>
2024-07-06 05:02:19 +0530

    simple react app

--- react_learning/src/App.js
+++ react_learning/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import FormText from './components/FormText';
 import Navbar from './components/Navbar';
 function App() {
   return (
     <>
     <Navbar title='TextUtls' about='Aboutt'/>
+    <div className='container my-3'>
+    <FormText heading='Enter your Text here'/>
+    </div>
     </>
   );
 }
